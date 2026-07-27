64ed7cba31c9031607b3fd976ce0e5046400263e
habumuremyisabbath2-stack <habumuremyisabbath2@gmail.com>
2026-07-27 09:49:03 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
-// App JavaScript
+const movies = [
+{
+title:"Rocky Saga",
+image:"https://via.placeholder.com/250x350?text=Rocky+Saga"
+},
+{
+title:"Fast X",
+image:"https://via.placeholder.com/250x350?text=Fast+X"
+},
+{
+title:"John Wick",
+image:"https://via.placeholder.com/250x350?text=John+Wick"
+}
+];
+
+const container = document.getElementById("movies");
+
+movies.forEach(movie=>{
+container.innerHTML += `
+<div class="movie-card">
+<img src="${movie.image}" alt="${movie.title}">
+<h3>${movie.title}</h3>
+<a href="watch.html">
+<button>Watch Now</button>
+</a>
+</div>
+`;
+});
